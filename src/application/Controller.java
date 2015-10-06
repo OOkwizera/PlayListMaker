dcd26698666c9e32121eb7bfd15c6804e1a871c8
SquirreLJ <leslieJD@hendrix.edu>
2015-10-06 16:20:18 -0500

    Here's the bloody Controller.

---
+++ src/application/Controller.java
@@ -0,0 +1,119 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+
+	private Catalog catalog = new Catalog();
+	private PlayList selectedPlayList;
+	private Song selectedSong;
+	
+	@FXML
+	private SplitPane canvas;
+	@FXML
+	private GridPane playListGrid;
+	@FXML
+	private GridPane songGrid;
+	
+	@FXML
+	private TextField enterTitle;
+	@FXML
+	private TextField enterSongName;
+	@FXML
+	private TextField enterArtist;
+	
+	@FXML 
+	private TableView<PlayList> catalogTable;
+	@FXML
+	private TableColumn<PlayList, String> playListTitle; 
+	@FXML 
+	private TableView<Song> playListTable;
+	@FXML
+	private TableColumn<Song, String> songTitle;
+	@FXML
+	private TableColumn<Song, String> songArtist;
+	
+	@FXML
+	public void initialize() {
+		
+		catalogTable.setItems(catalog.getCatalog());
+		
+		playListTitle.setCellValueFactory(
+				cellData -> cellData.getValue().titleProperty());
+		songTitle.setCellValueFactory( 
+				cellData -> cellData.getValue().getSongNameProperty());
+		
+		songArtist.setCellValueFactory(
+				cellData -> cellData.getValue().getArtistProperty());
+
+		
+		canvas.setOnKeyPressed(k -> handlePress(k.getCode()));
+		
+		playListTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> setSelectedSong(newValue));
+		
+		catalogTable.getSelectionModel().selectedItemProperty().addListener(
+				(observable, oldValue, newValue) -> setSelectedPlayList(newValue));
+	   
+	}
+	
+	public void setSelectedPlayList(PlayList playList) {
+		selectedPlayList = playList;
+		playListTable.setItems(selectedPlayList.getPlaylist());
+	}
+	
+	public void setSelectedSong(Song song) {
+		selectedSong = song;
+	}
+	
+	@FXML
+	public void handlePress(KeyCode code) {
+		if (code == KeyCode.ENTER) {
+			addPlayList();
+			addSong();
+		}
+		
+	}
+	
+	public void deletePlayList(){
+		if (selectedPlayList != null && catalog.getCatalog().contains(selectedPlayList)) {
+			catalog.removePlayList(selectedPlayList);
+		}
+	}
+	
+	public void deleteSong(){
+		if (selectedSong != null && selectedPlayList.getPlaylist().contains(selectedSong)) {
+			selectedPlayList.removeSong(selectedSong);
+		}
+	} 
+	
+	public void addPlayList(){
+		if (!enterTitle.getText().isEmpty()) {
+			PlayList playList = new PlayList(enterTitle.getText()); 
+			catalog.addPlayList(playList);
+			enterTitle.clear();
+		}
+	}
+	
+	public void editPlayList(){}
+	
+	public void addSong(){ 
+		if (!enterSongName.getText().isEmpty()) {
+			Song song = new Song(enterSongName.getText(), enterArtist.getText()); 
+			selectedPlayList.addSong(song);
+			enterSongName.clear();
+			enterArtist.clear();
+		}
+	}
+	
+	public void editSong(){}
+	
+	public void sharePlayList(){}
+	
+}

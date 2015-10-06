710d7d6c23a3889e353d977a7f32294a1ea79659
kvgarimella <kvgarimella@gmail.com>
2015-10-06 16:40:37 -0500

    Check out the changes.

--- src/application/net/Server.java
+++ src/application/net/Server.java
@@ -1,9 +1,12 @@
 package application.net;
 
+import application.*;
 public class Server {
 
 	public Server() {
 		// TODO Auto-generated constructor stub
+		
+		PlayList p = new PlayList("hello");
 	}
 
 }

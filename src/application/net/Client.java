9d44d57ae66dc14dfc58bb8e5ed90b2eb3a4c007
kvgarimella <kvgarimella@gmail.com>
2015-10-06 23:19:31 -0500

    Adding the Networking Capabilities.

---
+++ src/application/net/Client.java
@@ -0,0 +1,21 @@
+package application.net;
+
+import java.io.IOException;
+import java.net.*;
+import java.io.*;
+public class Client {
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        String name = args[0];
+        String port = args[1];
+        int intport = Integer.parseInt(port);
+        PrintWriter printWriter;
+        Socket socket = new Socket(InetAddress.getByName("ozark.hendrix.edu"), intport);
+        printWriter = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader bufferedReader = new java.io.BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String readerInput = bufferedReader.readLine();
+            printWriter.println(name + ": " + readerInput);
+        }
+
+    }
+}
--- src/application/net/Server.java
+++ src/application/net/Server.java
@@ -1,13 +1,33 @@
 package application.net;
 
-import application.*;
 
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Random;
+
+/**
+ * Created by kvgarimella on 10/6/15.
+ */
 public class Server {
-
-	public Server() {
-		// TODO Auto-generated constructor stub
-		
-		PlayList p = new PlayList("hello");
+	Random r = new Random();
+	int port = 4445;
+	public static void main (String[] args) throws IOException {
+		new Server().runServer();
 	}
 
+	public void runServer() throws IOException {
+		int port = 9400;
+		ServerSocket serverSocket = new ServerSocket(port);
+		InetAddress ip = serverSocket.getInetAddress();
+		System.out.println("this is my ip: " + ip);
+		System.out.println("Server up & Ready for connections at port.. " + port);
+		while (true) {
+			Socket socket = serverSocket.accept();
+			new ServerThread(socket).start();
+		}
+
+
+
+	}
 }
